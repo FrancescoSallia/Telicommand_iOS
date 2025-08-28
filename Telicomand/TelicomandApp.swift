e258cd2cdfba0797ab510ee7f8386e4b10451e26
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-08-28 15:43:01 +0200

    Initial Commit

---
+++ Telicomand/TelicomandApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelicomandApp.swift
+//  Telicomand
+//
+//  Created by Francesco Sallia on 28.08.25.
+//
+
+import SwiftUI
+
+@main
+struct TelicomandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
